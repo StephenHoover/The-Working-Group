52a273d45fd0d481f9705e6236ab4cdf1d6cd097
Stephen Hoover <32989727+StephenHoover@users.noreply.github.com>
2022-10-14 12:50:44 -0400

    Update QR-Code.py

--- QR-Code.py
+++ QR-Code.py
@@ -2,19 +2,25 @@
 import qrcode
 import qrcode.image.svg
 
+# Add your URL
 data = "www.stephenhoover.org"
 
-import qrcode
+# Define shape, border and error correction
 qr = qrcode.QRCode(
     version=1,
     error_correction=qrcode.constants.ERROR_CORRECT_L,
     box_size=10,
     border=4,
 )
+
+# Encode Data (URL) into QR
 qr.add_data(data)
 qr.make(fit=True)
 
+# Change Colors
 img = qr.make_image(fill_color=(0, 51, 102), back_color="white")
+
+# Format as vector image 
 qrcode.image.svg.SvgFillImage
 
 # Save as PNG
